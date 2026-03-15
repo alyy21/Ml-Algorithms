431bb38fcb0ebc841dc34b25ce4bbf6500fd26b7
alyy21 <gwl5179@gmail.com>
2026-03-15 15:32:35 +0900

    logistic regression model training

--- logistic_regression.py
+++ logistic_regression.py
@@ -54,4 +54,102 @@ for col in df.columns:
     if(df[col].dtype == 'object'):
         df[col]= le.fit_transform(df[col])
     
-print(df)
+# print(df)
+
+
+
+# VIF
+x = df.drop('y',axis = 1)
+y = df['y']
+# print(x)
+# print(y)
+
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+vif_values =[]
+for i in range(len(x.columns)):
+    vif = variance_inflation_factor(x.values,i)
+    vif_values.append(vif)
+
+vif_df = pd.DataFrame()
+vif_df ['Features']= x.columns
+vif_df['Multocollinearity']=vif_values
+# print(vif_df)
+
+
+x.drop('nr.employed', axis = 1, inplace=True)
+vif_df = pd.DataFrame()
+vif_df ['Features']= x.columns
+vif_values =[]
+for i in range(len(x.columns)):
+    vif = variance_inflation_factor(x.values,i)
+    vif_values.append(vif)
+vif_df['Multocollinearity']=vif_values
+# print(vif_df)
+
+x.drop('cons.price.idx', axis = 1, inplace=True)
+vif_df = pd.DataFrame()
+vif_df ['Features']= x.columns
+vif_values =[]
+for i in range(len(x.columns)):
+    vif = variance_inflation_factor(x.values,i)
+    vif_values.append(vif)
+vif_df['Multocollinearity']=vif_values
+# print(vif_df)
+
+
+x.drop('pdays', axis = 1, inplace=True)
+vif_df = pd.DataFrame()
+vif_df ['Features']= x.columns
+vif_values =[]
+for i in range(len(x.columns)):
+    vif = variance_inflation_factor(x.values,i)
+    vif_values.append(vif)
+vif_df['Multocollinearity']=vif_values
+# print(vif_df)
+
+x.drop('euribor3m', axis = 1, inplace=True)
+vif_df = pd.DataFrame()
+vif_df ['Features']= x.columns
+vif_values =[]
+for i in range(len(x.columns)):
+    vif = variance_inflation_factor(x.values,i)
+    vif_values.append(vif)
+vif_df['Multocollinearity']=vif_values
+# print(vif_df)
+
+
+x.drop('cons.conf.idx', axis = 1, inplace=True)
+vif_df = pd.DataFrame()
+vif_df ['Features']= x.columns
+vif_values =[]
+for i in range(len(x.columns)):
+    vif = variance_inflation_factor(x.values,i)
+    vif_values.append(vif)
+vif_df['Multocollinearity']=vif_values
+# print(vif_df)
+
+x.drop('age', axis = 1, inplace=True)
+vif_df = pd.DataFrame()
+vif_df ['Features']= x.columns
+vif_values =[]
+for i in range(len(x.columns)):
+    vif = variance_inflation_factor(x.values,i)
+    vif_values.append(vif)
+vif_df['Multocollinearity']=vif_values
+# print(vif_df)
+
+
+# Model Training 
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.80)
+from sklearn.linear_model import LogisticRegression
+lr=LogisticRegression()
+lr.fit(x_train,y_train)
+y_pred=lr.predict(x_test)
+# print(y_pred)
+# print(y_test)
+from sklearn.metrics import *
+ac = accuracy_score(y_test,y_pred)*100
+cf=confusion_matrix(y_test,y_pred)
+print(cf)
+print(ac)
